bfd00a7061e1eca43107586858ddb69a16fe7795
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-09-05 20:42:06 +0300

    ProductScreen Page partially completed

--- frontend/src/screens/ProductScreen/ProductScreen.js
+++ frontend/src/screens/ProductScreen/ProductScreen.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../../components/Rating/Rating';
+import products from '../../products';
+import { Fragment } from 'react';
 
-const ProductScreen = () => {
-  return <div>THIS IS PRODUCT SCREEN</div>;
+const ProductScreen = ({ match }) => {
+  const product = products.find((product) => product._id === match.params.id);
+
+  return (
+    <Fragment>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews.`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price : €{product.price}</ListGroup.Item>
+            <ListGroup.Item>Description : {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price : </Col>
+                  <Col>
+                    <strong>€{product.price}</strong>{' '}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status : </Col>
+                  <Col
+                    style={{ color: product.countInStock === 0 ? 'red' : '' }}
+                  >
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}{' '}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              {product.countInStock !== 0 && (
+                <ListGroup.Item>
+                  <Button className='btn-block w-100' type='button'>
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              )}
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </Fragment>
+  );
 };
 
 export default ProductScreen;
