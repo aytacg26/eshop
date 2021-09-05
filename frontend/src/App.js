455621c1a003e0a97b307679e0db9bd960e44337
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-09-05 16:08:55 +0300

    Header, Footer and Layout structure completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
-  return <div>MERN eShop</div>;
+  return (
+    <Fragment>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to MERN eShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 
@@ -7,5 +8,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
