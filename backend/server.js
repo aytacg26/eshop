501b4036d9dd7278435713d33a8cd3432f23fcac
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-09-06 21:56:47 +0300

    Initial Server Setup Completed

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = 5000;
+app.listen(PORT, console.log(`Server is up and running at port ${PORT}`));
