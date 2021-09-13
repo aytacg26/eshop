208271b7005013d5429f23c073597ecf9c1923cb
aytacguley <aytacguley@dklassix.com>
2021-09-13 22:39:39 +0300

    init quantity state changed to 1

--- frontend/src/screens/ProductScreen/ProductScreen.js
+++ frontend/src/screens/ProductScreen/ProductScreen.js
@@ -17,7 +17,7 @@ import Loader from '../../components/Loader/Loader';
 import Message from '../../components/Message/Message';
 
 const ProductScreen = ({ history, match }) => {
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(1);
 
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
