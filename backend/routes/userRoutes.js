4eaec33cdd94aeeab63f2ffb316fef0255bd4084
aytacguley <aytacguley@dklassix.com>
2021-09-18 18:04:26 +0300

    profile put request route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post('/', registerUser);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').put(protect, updateUserProfile);
 
 export default router;
