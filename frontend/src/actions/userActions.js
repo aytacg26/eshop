445f35668b044f353005e20238fa05fc65f24261
aytacguley <aytacguley@dklassix.com>
2021-09-18 16:09:55 +0300

    Regşstration Actions and Reducer created

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,7 +4,26 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_FORM_ERROR_CLEAN,
 } from '../constants/userConstants';
+
+const formError = (dispatch, actionType, error) => {
+  dispatch({
+    type: actionType,
+    payload:
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message,
+  });
+
+  const timer = setTimeout(() => {
+    dispatch({ type: USER_FORM_ERROR_CLEAN });
+    clearTimeout(timer);
+  }, 5000);
+};
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -28,13 +47,48 @@ export const login = (email, password) => async (dispatch) => {
 
     localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
+    // dispatch({
+    //   type: USER_LOGIN_FAIL,
+    //   payload:
+    //     error.response && error.response.data.message
+    //       ? error.response.data.message
+    //       : error.message,
+    // });
+    formError(dispatch, USER_LOGIN_FAIL, error);
+  }
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
     dispatch({
-      type: USER_LOGIN_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
+      type: USER_REGISTER_REQUEST,
     });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    );
+
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    // dispatch({
+    //   type: USER_REGISTER_FAIL,
+    //   payload:
+    //     error.response && error.response.data.message
+    //       ? error.response.data.message
+    //       : error.message,
+    // });
+    formError(dispatch, USER_REGISTER_FAIL, error);
   }
 };
 
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,10 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_FORM_ERROR_CLEAN,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -16,8 +20,29 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_FAIL:
       return { loading: false, error: payload };
 
+    case USER_FORM_ERROR_CLEAN:
+      return { error: null };
+
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: payload };
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: payload };
+    case USER_FORM_ERROR_CLEAN:
+      return { error: null };
+
     default:
       return state;
   }
