a464e3c8adf2c1e9155f12eb101da19ff8411b17
aytacguley <aytacguley@dklassix.com>
2021-09-19 13:00:59 +0300

    Route to Profile Screen added to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductScreen from './screens/ProductScreen/ProductScreen';
 import CartScreen from './screens/CartScreen/CartScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen/ProfileScreen';
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
             <Route path='/cart/:id?' component={CartScreen} />
             <Route path='/signin' component={LoginScreen} />
             <Route path='/register' component={RegisterScreen} />
+            <Route path='/profile' component={ProfileScreen} />
           </Switch>
         </Container>
       </main>
