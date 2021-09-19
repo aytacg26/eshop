4df5d0f8e7417d854368c06f2780baac816d2d5a
aytacguley <aytacguley@dklassix.com>
2021-09-19 13:00:14 +0300

    Profile Update and Details Reducers created

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -7,6 +7,12 @@ import {
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
   USER_FORM_ERROR_CLEAN,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -39,6 +45,42 @@ export const userRegisterReducer = (state = {}, action) => {
     case USER_REGISTER_SUCCESS:
       return { loading: false, userInfo: payload };
     case USER_REGISTER_FAIL:
+      return { loading: false, error: payload };
+    case USER_FORM_ERROR_CLEAN:
+      return { error: null };
+
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: payload };
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: payload };
+    case USER_FORM_ERROR_CLEAN:
+      return { error: null };
+
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true, userInfo: payload };
+    case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: payload };
     case USER_FORM_ERROR_CLEAN:
       return { error: null };
